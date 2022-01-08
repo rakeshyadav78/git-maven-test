c2dfdf7421b4cb6f18665a485a64ac481a19281c
Rakeshkumar Yadav <yyyrakesh@gmail.com>
2022-01-08 09:34:03 +0530

    Method added

--- src/main/java/com/tgd/test/Test.java
+++ src/main/java/com/tgd/test/Test.java
@@ -1,15 +1,20 @@
 package com.tgd.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
+import com.tgd.test.util.TestUtil;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class Test {
+	private static TestUtil testUtil = TestUtil.getInstance();
 
 	public static void main(String[] args) {
-		log.info("Main Method Running.");
-		List<String> list=new ArrayList<String>();
+		HashMap<String, Integer> hashMap = testUtil.checkCharOccurence("Raakeshs");
+		log.info("Map Size : " + hashMap.size());
+		hashMap.entrySet().stream().forEach(entry -> {
+			System.out.println("Key : " + entry.getKey() + ", Value : " + entry.getValue());
+		});
 	}
 }
